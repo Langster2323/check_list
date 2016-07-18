09e2500cc20bacfc03a03721a3764f90f5e8bbc2
Ahkeem Lang <langahkeem@gmail.com>
2016-07-18 18:20:29 -0500

    Finished controllers and model... working on the view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,13 @@
 class ListsController < ApplicationController
+  def index
+    render locals: {
+      lists: List.all?
+    }
+  end
+
+  def show
+    render locals: {
+      list: List.find(params[:id])
+    }
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,31 @@
 class TasksController < ApplicationController
+  def index
+    render locals: {
+      tasks: Task.where(list_id: params[:list_id]),
+      list: List.find(params[list_id])
+    }
+  end
+
+  def show
+    render locals: {
+      task: Task.find(params[:id])
+    }
+  end
+
+  def new
+    render locals: {
+      task: Task.new,
+      list: List.find(params[:id])
+    }
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.list_id = params[:list_id]
+    if task.save
+      redirect_to_list_task_path(params[:list_id], task)
+    else
+      render :new
+    end
+  end
 end
