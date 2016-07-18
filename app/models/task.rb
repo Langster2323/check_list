c84fe2b21b8fe6793d46797f6cee2a00bad93fa9
Ahkeem Lang <langahkeem@gmail.com>
2016-07-18 16:52:56 -0500

    completed my validations for my models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
+  validate :body, presence: true
+  validate :list_id, presence: true
+  validate :completed, presence: true
+
   belongs_to :list
+  has_many :list_id
 end
